e7e7967352aa9ae010a080e4533782a547ceac60
Asad Momin <asadmomin8768@gmail.com>
2026-08-12 21:54:17 +0530

    .md file needed.

---
+++ bmi_calc.js
@@ -0,0 +1,28 @@
+function calculateBMI () {
+
+    let height = document.getElementById("height").value;
+
+    let weight = document.getElementById("weight").value;
+
+    height = height / 100;
+
+    let bmi = weight / (height * height);
+
+    let status = "";
+
+    if (bmi < 18.5) {
+        status = "Underweight";
+    }
+    else if (bmi < 25) {
+        status = "Normal Weight";
+    }
+    else if (bmi < 30) {
+        status = "Overweight";
+    }
+    else {
+        status = "Obese";
+    }
+
+document.getElementById("result").innerHTML = "BMI = " + bmi.toFixed(2) + "<br>Status = " + status;
+
+} 
